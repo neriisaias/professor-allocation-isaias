af1bdcddd956499b0409f9b1095561741c0a05b6
Isaias Neri <isaias_neri@hotmail.com>
2024-02-24 14:29:15 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/isaias/ProfessorAllocationIsaiasApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.isaias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationIsaiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationIsaiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/isaias/ProfessorAllocationIsaiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.isaias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationIsaiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
